6030197144df7c476ef0e9365c8d70a929912c23
degi100 <r.degering@mail.de>
2021-09-30 15:03:41 +0200

    add jsondata

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import './App.scss';
-import links from './data/links.json';
+import React, { useState, useEffect } from "react";
+import feed from "./data/feeds.json"
+import "./App.scss";
 
 function App() {
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    feed.forEach(feed => {
+    (async () => {
+      const response = await fetch(feed.linksUrl);
+      const tempLinks = await response.json();
+      tempLinks.forEach(tempLink => tempLink.origin = feed.name);
+      setLinks(n => [...n, ...tempLinks]);
+     })();
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1>Links</h1>    
+      <h1>Links</h1>
       <ul>
-      {links.map((link, index)=> {
-        return <li key={index}><a target="_blank" href={links.url} rel="noreferrer">{link.title}  </a></li>
-      })}
+        {links.map((link, index) => {
+          return (
+            <li key={index}>
+              <a target="_blank" href={links.url} rel="noreferrer">
+                {link.title} (from {link.origin})
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
-
   );
 }
 
